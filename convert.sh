7ca6fb333dad3cc2f953c11448d7406870858964
Doga Gulhan <dgulhan@dogas-mbp.private.wireless.med.harvard.edu>
2017-05-04 11:08:15 -0400

    add the Rda conversion

--- convert.sh
+++ convert.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+#strip the list of snvs
 for file in *
   do awk '{print $6}' $file > snv_$file
 done
@@ -10,10 +11,12 @@ rm *
 mv snv/* .
 rm -r snv
 
+#remove paranthesis make lower case
 for file in *
   do sed -e 's/\[/p/g' $file| sed -e 's/\]/p/g' | sed -e 's/>/to/g' | awk '{print tolower($0)}' > lower_$file
 done
 
+#change vocabulary to a simpler format
 for file in *
   do for i in {1..96}
     do alp=`sed ''$i'!d' ../vocabAlp.txt`
@@ -24,4 +27,32 @@ for file in *
  done
 done
 
-rm *.sh
+rm -f snv_*
+rm -f *convert.sh
+
+#remove the first line
+for file in *
+  do  tail -n +2 $file > test.tmp
+  mv -f test.tmp $file
+done
+
+touch writeToR.R
+echo 'frame3BaseAging<-data.frame(c("' > writeToR.R
+
+nfile=`ls lower_snv_* | wc -l`
+count=1
+for file in lower*
+  do sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g' $file >> writeToR.R
+  if [ $count -lt $nfile ]; then
+     echo '","' >> writeToR.R
+  fi
+  let count=$count+1
+  echo $count
+done
+echo '"))' >> writeToR.R
+
+rm -f test.tmp
+sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g' writeToR.R > test.tmp
+mv -f test.tmp  writeToR.R
+
+echo 'save(frame3BaseAging,file="frame3BaseAging.Rda")' >> writeToR.R
