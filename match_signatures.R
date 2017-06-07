8675a6deb67075b8c31ff756b0fc5abeee1e0119
Doga Gulhan <dcgulhan@gmail.com>
2017-06-07 13:32:36 -0400

    mc comp

--- match_signatures.R
+++ match_signatures.R
@@ -1,17 +1,23 @@
-match_signatures<-function(signatures, signatures_min){
-  index = rep(0,5)
-  largest_similarity = rep(0,5)
+match_signatures<-function(signatures, signatures_new){
+  index <- rep(0,5)
+  largest_similarity <- rep(0,5)
+  nsig <- dim(signatures)[[2]]
+  
   for(i in 1:nsig){
-    max = 0
+    max <- 0
+    if(sum(signatures_new[,i]) == 0){
+      index[[i]] <- (-1)
+      next
+    }
     for(j in 1:nsig){
-      similarity = signatures_min[,i]%*%signatures[,j]/sqrt(signatures_min[,i]%*%signatures_min[,i]*signatures[,j]%*%signatures[,j])
-      print(sprintf('%d %d %.2f',i, j, similarity))
+      similarity <- signatures_new[,i]%*%signatures[,j]/sqrt(signatures_new[,i]%*%signatures_new[,i]*signatures[,j]%*%signatures[,j])
+      print(sprintf('%d %d %.2f %.6f %.6f',i, j, similarity, (signatures_new[,i]%*%signatures_new[,i]), (signatures[,j]%*%signatures[,j])))
       if(similarity > max){
-        max = similarity
-        index[[i]] = j
+        max <- similarity
+        index[[i]] <- j
       }
     }
-    largest_similarity[[i]] = max
+    largest_similarity[[i]] <- max
   }
   return(index)
 }
