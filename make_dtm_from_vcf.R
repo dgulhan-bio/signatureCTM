7c76a70745397d0f2ac9191c3dabfd89049f7243
Doga Gulhan <dgulhan@Dogas-MacBook-Pro.local>
2017-08-27 09:38:57 -0400

    bug fix

--- make_dtm_from_vcf.R
+++ make_dtm_from_vcf.R
@@ -36,17 +36,27 @@ library(BSgenome.Hsapiens.UCSC.hg19)
     }else{
       ref <- .flip_base(ref)
       alt <- .flip_base(substr(snv, 2, 2))
-      prime5 <- .flip_base(substr(snv, 3, 3))
-      prime3 <- .flip_base(substr(snv, 4, 4))
-      comb_out[[isnv]] <- paste0(ref, alt, prime3, prime5)
+      ext <- substr(snv, 3, nchar(snv))
+
+      #every base after the first two belongs to context
+      for(iext in 1:nchar(ext)){
+        replacement <- .flip_base(substr(ext, iext, iext))
+        if(!exists('flip_ext')) flip_ext <- replacement 
+        else flip_ext <- paste0(flip_ext, replacement)
+      }
+
+      comb_out[[isnv]] <- paste0(ref, alt, flip_ext)
+      rm(flip_ext)
     }
   }
+
   return(comb_out)
 }
 
 ##given nmer context, ref, alt information calculates 
 ##counts for each type of snv
 .convert_seq_to_vector <- function(context, ref_vector, alt_vector, types, nstrand = 1){
+  
   context <- tolower(context)
   ref_vector <- tolower(ref_vector)
   alt_vector <- tolower(alt_vector)
@@ -61,18 +71,15 @@ library(BSgenome.Hsapiens.UCSC.hg19)
   range <- nchar(context[[1]])
   if(range %% 2 == 0)
     stop('context should be an odd number')
-  
- 
   combined <- paste0(ref_vector, alt_vector) 
   combined <- paste0(combined, substr(context, 1, floor(range/2)))
   combined <- paste0(combined, substr(context, ceiling(range/2)+1, range))
-  
 
   if(nstrand == 1) combined <- .flip_strand(combined)
   
 
   count_vector <- rep(0, length(types))
- 
+
   #binary search for counting the occurances of each type
   for(snv in combined){
     L <- 1
@@ -98,19 +105,47 @@ library(BSgenome.Hsapiens.UCSC.hg19)
   base_in <- ''
   if(nstrand == 1) base_in <- c('c', 't')
   else base_in <- components  
-  types <- rep('', 6*4^(ncontext -1)*nstrand )
+#  types <- rep('', 6*4^(ncontext -1)*nstrand )
+ 
+  nextend <- (ncontext - 1)
   index <- 1
+  
+  #first construct types without context
+  #first factor is count of possible alt
+  #second factor is count of possible ref
+  types <- rep ('', (length(components) -1) * length(base_in)) 
+
   for(base in base_in){
-    for(alt in components[!grepl(base,components)]){
-      for(prime5 in components){
-        for(prime3 in components){     
-          types[[index]] <- paste0(base, alt, prime5, prime3)
-           index <- index + 1
-        }
-      }
+    alt_out <- components[!grepl(base, components)]
+    for(alt in alt_out){
+      types[[index]] <- paste0(base, alt)
+      index <- index + 1
     }
   }
+
+  types <- rep(types, length(components)^nextend) 
   types <- sort(types)
+ 
+  for(iextend in 1:nextend){
+    
+    length_rep <- length(components)^(nextend - iextend)
+
+    nrep <- length(components)^(iextend - 1) * (length(components) -1) * length(base_in)
+
+    for(irep in 1:nrep){
+      for(component in components){
+        if(!exists("types_tmp")) types_tmp <- rep(component, length_rep)
+        else types_tmp <- c(types_tmp, rep(component, length_rep))
+      }
+    }
+    for(index in 1:length(types)){
+      types[[index]] <- paste0(types[[index]], types_tmp[[index]])
+    }
+    rm(types_tmp)
+  }
+
+  types <- sort(types)
+  return(types)
 }
 
 ##converts a matrix to a document term matrix
@@ -118,10 +153,11 @@ make_dtm_from_matrix <- function(input_matrix, sample_names, types){
   dtm <- as.simple_triplet_matrix(t(input_matrix))
   dtm$dimnames$Docs <- as.character(1:length(sample_names))
   #dtm$dimnames$Docs <- sample_names
+  #print(types)
   dtm$dimnames$Terms <- types
+
   class(dtm) <- c("DocumentTermMatrix", "simple_triplet_matrix")
   attr(dtm, "weighting") <- c("term frequency", "tf")
-  print(dim(input_matrix))
   return(dtm)
 }
 
@@ -154,14 +190,14 @@ make_vector_from_vcf <- function(vcf_file, ref_genome = BSgenome.Hsapiens.UCSC.h
 make_matrix_from_vcf_list <- function(vcf_file_list, ref_genome = BSgenome.Hsapiens.UCSC.hg19, ncontext = 3, nstrand = 1){
   vcf_files_table <- read.table(vcf_file_list)
   vcf_files <- as.character(vcf_files_table$V1)
-  matrix_snvs <- matrix(0, 6*4^(ncontext - 1)*nstrand, length(vcf_files))
+  matrix_snvs <- matrix(0, 6*4^(ncontext - 1)*nstrand, length(vcf_files)) 
   types <- .make_type(ncontext, nstrand)
   for(ifile in 1:length(vcf_files)){
     count_vector <- make_vector_from_vcf(vcf_files[[ifile]], ref_genome, types, ncontext, nstrand)
     matrix_snvs[, ifile] <- count_vector
   }  
-  rownames(matrix_snvs, types)
-  colnames(matrix_snvs, vcf_files)
+  rownames(matrix_snvs) <- types
+  colnames(matrix_snvs) <- vcf_files
   return(matrix_snvs)
 }
 
@@ -175,6 +211,8 @@ make_dtm_from_vcf_list <- function(vcf_file_list, ref_genome = BSgenome.Hsapiens
     count_vector <- make_vector_from_vcf(vcf_files[[ifile]], ref_genome, types, ncontext, nstrand)
     matrix_snvs[, ifile] <- count_vector
   }  
+  print('end of make dtm from vcf list')
+  #print(types)
   return(make_dtm_from_matrix(matrix_snvs, vcf_files, types))
 }
 
